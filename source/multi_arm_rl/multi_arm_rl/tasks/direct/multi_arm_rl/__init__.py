9dae0bfe412c6b80850ad744401482699075e0f1
yuepeng <brand960@foxmail.com>
2025-12-09 10:39:25 +0800

    Initial commit

---
+++ source/multi_arm_rl/multi_arm_rl/tasks/direct/multi_arm_rl/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Multi-Arm-Rl-Direct-v0",
+    entry_point=f"{__name__}.multi_arm_rl_env:MultiArmRlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.multi_arm_rl_env_cfg:MultiArmRlEnvCfg",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
